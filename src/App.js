3a4effd9637f4f46a850944d24a80fb05a88d254
Nethmi Anjani <anjaninethmi23@gmail.com>
2024-02-15 02:03:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
 import "./App.css";
 import Login from "./Components/Login/Login";
-import SignUp from "./Components/SignUp/SignUp"
-
+import SignUp from "./Components/SignUp/SignUp";
+import SideBar from "./Components/Pages/SideBar/SideBar";
+import Patient from "./Components/Pages/Patient/Patient";
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
 
           <Route path="/createAccount" element={<SignUp/>} />
+        </Routes>
+      </Router> */}
+      {/* <BrowserRouter>
+        <SideBar>
+          <Routes>
+            <Route path="/patient" element={<Patient />} />
+          </Routes>
+        </SideBar>
+      </BrowserRouter> */}
+      <Router>
+        <Routes>
+          {/* <Route exact path="/" element={<Login />} /> */}
+
+          <Route exact path="/" element={<Patient />} />
+          {/* <Route path="/createAccount" element={<SignUp />} /> */}
         </Routes>
       </Router>
     </div>
--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { Component, useState } from "react";
 import PersonIcon from "@mui/icons-material/Person";
 import CallIcon from "@mui/icons-material/Call";
 import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
 import KeyIcon from "@mui/icons-material/Key";
 import EmailIcon from "@mui/icons-material/Email";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
 import "./SignUp.css";
 
-const SignUp = () => {
-  const handleSubmit = (e) => {
+function SignUp() {
+  let navigate = useNavigate();
+
+  const [PatientDetails, setPatientDetails] = useState({
+    name: "",
+    email: "",
+    gender: "-1",
+    mobile: "",
+    password: "",
+    rePassword: "",
+  });
+
+  const { name, email, gender, mobile, password, rePassword } = PatientDetails;
+
+  const onInputChange = (e) => {
+    setPatientDetails({
+      ...PatientDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmit = async (e) => {
     e.preventDefault();
+
+    if (password !== rePassword) {
+      alert("Passwords do not match!");
+      return;
+    }
+
+    await axios.post("http://localhost:8080/patients", PatientDetails);
+    navigate("/");
   };
 
   return (
     <div>
       <div className="sign-up-page">
         <div className="form-board-body card">
-          <form action="/" onSubmit={handleSubmit}>
+          <form action="/" onSubmit={(e) => onSubmit(e)}>
             <div className="info-set">
               <div className="info-item">
                 <label className="icon">
@@ -26,7 +56,9 @@ const SignUp = () => {
                   type="text"
                   name="name"
                   id="name"
+                  value={name}
                   placeholder="Name"
+                  onChange={(e) => onInputChange(e)}
                   required
                 />
               </div>
@@ -39,7 +71,9 @@ const SignUp = () => {
                   type="text"
                   name="email"
                   id="email"
+                  value={email}
                   placeholder="Email"
+                  onChange={(e) => onInputChange(e)}
                   required
                 />
               </div>
@@ -52,7 +86,9 @@ const SignUp = () => {
                   type="text"
                   name="mobile"
                   id="mobile"
+                  value={mobile}
                   placeholder="Mobile"
+                  onChange={(e) => onInputChange(e)}
                   required
                 />
               </div>
@@ -60,7 +96,12 @@ const SignUp = () => {
                 <label className="icon">
                   <PersonOutlineIcon />
                 </label>
-                <select name="Gender" className="input-tags">
+                <select
+                  name="gender"
+                  className="input-tags"
+                  value={gender}
+                  onChange={(e) => onInputChange(e)}
+                >
                   <option value="-1">Gender</option>
                   <option value="Male">Male</option>
                   <option value="Female">Female</option>
@@ -74,8 +115,10 @@ const SignUp = () => {
                   className="input-tags"
                   type="password"
                   name="password"
+                  value={password}
                   id="password"
                   placeholder="Password"
+                  onChange={(e) => onInputChange(e)}
                   required
                 />
               </div>
@@ -86,14 +129,14 @@ const SignUp = () => {
                 <input
                   className="input-tags"
                   type="password"
-                  name="re-password"
-                  id="re-password"
+                  name="rePassword"
+                  id="rePassword"
                   placeholder="Re-Password"
+                  
                   required
                 />
               </div>
             </div>
-
             <div>
               <button type="submit" className="register-button">
                 Register
@@ -104,5 +147,6 @@ const SignUp = () => {
       </div>
     </div>
   );
-};
+}
+
 export default SignUp;
